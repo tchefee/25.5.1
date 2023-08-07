f82f5a2f129a806a9d3ac89a2c7c458500c371e0
Iurii Trokhachev <126320741+tchefee@users.noreply.github.com>
2023-08-07 14:34:36 +0300

    Create test_show_pet_friends.py

---
+++ tests_pets/test_show_pet_friends.py
@@ -0,0 +1,43 @@
+import pytest
+from key import valid_email, valid_password
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+
+def test_show_pet_friends():
+   '''Проверка карточек питомцев'''
+
+   # Устанавливаем неявное ожидание
+   pytest.driver.implicitly_wait(10)
+
+   # Вводим email
+   pytest.driver.find_element(By.ID, 'email').send_keys(valid_email)
+
+   # Вводим пароль
+   pytest.driver.find_element(By.ID, 'pass').send_keys(valid_password)
+
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.current_url == 'http://petfriends1.herokuapp.com/all_pets'
+
+   images = pytest.driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
+   names = pytest.driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
+   descriptions = pytest.driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
+
+   assert names[0].text != ''
+
+   for i in range(len(names)):
+      assert images[i].get_attribute('src') != ''
+      assert names[i].text != ''
+      assert descriptions[i].text != ''
+      assert ',' in descriptions[i].text
+      parts = descriptions[i].text.split(", ")
+      assert len(parts[0]) > 0
+      assert len(parts[1]) > 0
+
+# python -m pytest -v --driver Chrome --driver-path /tests_drivers/chromedriver.exe tests/test_show_pet_friends.py
